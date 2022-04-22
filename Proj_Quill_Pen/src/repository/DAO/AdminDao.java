462bfb0993ee25c9049de7207493bceafaef766c
Seojw <jeongwon4103@daum.net>
2022-04-22 21:57:55 +0900

    add adminMapper, AdminDao 0422

--- Proj_Quill_Pen/src/repository/DAO/AdminDao.java
+++ Proj_Quill_Pen/src/repository/DAO/AdminDao.java
@@ -1,100 +1,186 @@
 package repository.DAO;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import myBatis.MyBatis;
+import repository.DTO.BookBean;
+import repository.DTO.DeclBean;
 import repository.DTO.NoticeBean;
 
 public class AdminDao {
+	private static String namespace = "myBatis.mapper.adminMapper.";
+	
 	SqlSessionFactory sqlSessionFactory = MyBatis.getSqlSessionFactory();
 
-	/////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////// 관리자 유효성 검사 시작///////////////////////////////
 	// 해당 아이디의 등급 가져오기 (select)
-	public boolean isAdmin(String mId) throws Exception {
+	public boolean isAdmin(String uId) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		
 		boolean check = false;
 		
 		try {
 			// 결과가 1이면 true 아니면 false
-			// int result = sqlSession."매퍼(mapper)에서 설정한 id"(매개변수, ...);
+			int result = sqlSession.selectOne(namespace+"isAdmin", uId);
+			check = result == 1 ? true : false;
+
 			return check; 
 		} finally {
 			sqlSession.close();
 		}
 	}
 	//////////////////////////// 관리자 유효성 검사 끝////////////////////////////////
-	/////////////////////////////////////////////////////////////////////////////////
 	
-	/////////////////////////////////////////////////////////////////////////////////
 	///////////////////////// 공지사항 목록 CRUD 시작 //////////////////////////////
-	// 공지사항 목록 가져오기 (select)
-	public List<NoticeBean> selectNoticeList(String mId) throws Exception {
+	// 공지사항 목록 가져오기 (select)   pageNo : 페이지 번호
+	public List<NoticeBean> selectNoticeList(int pageNo) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		
+		pageNo = (pageNo-1)*5;
 		
 		try {
-			// int result = sqlSession."매퍼(mapper)에서 설정한 id"(매개변수, ...);
-			return new ArrayList<NoticeBean>(); 
+			return sqlSession.selectList(namespace+"selectNoticeList", pageNo); 
 		} finally {
 			sqlSession.close();
 		}
 	}
 	///////////////////////// 공지사항 목록 CRUD 끝 ////////////////////////////////
-	/////////////////////////////////////////////////////////////////////////////////
 	
-	/////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////// 공지글 CRUD 시작//////////////////////////////////
 	// 공지 글 쓰기(insert)
-	public int insertNotice(String mId, String title, String content) throws Exception {
+	public int insertNotice(NoticeBean bean) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		try {
-			return 1; // sqlSession."매퍼(mapper)에서 설정한 id"(매개변수, ...);
+			return sqlSession.insert(namespace+"insertNotice", bean);
 		} finally {
 			sqlSession.close();
 		}
 	}
 
 	// 공지 글 가져오기(select)
-	public NoticeBean selectNotice(String mId, int bno) throws Exception {
+	public NoticeBean selectNotice(int bno) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
-
+		
 		try {
-			return new NoticeBean(); // sqlSession."매퍼(mapper)에서 설정한 id"(매개변수, ...);
+			return sqlSession.selectOne(namespace+"selectNotice", bno);
 		} finally {
 			sqlSession.close();
 		}
 	}
 
-	// 공지 글 수정하기(select)
-	public int updateNotice(String mId, int bno, String title, String content) throws Exception {
+	// 공지 글 수정하기(update)
+	public int updateNotice(NoticeBean bean) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		try {
-			return 1; // sqlSession."매퍼(mapper)에서 설정한 id"(매개변수, ...);
+			return sqlSession.update(namespace+"updateNotice", bean);
 		} finally {
 			sqlSession.close();
 		}
 	}
 
 	// 공지 글 삭제하기(delete)
-	public int deleteNotice(String mId, int bno) throws Exception {
+	public int deleteNotice(int bno) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		try {
-			return 1; // sqlSession."매퍼(mapper)에서 설정한 id"(매개변수, ...);
+			return sqlSession.delete(namespace+"deleteNotice", bno);
 		} finally {
 			sqlSession.close();
 		}
 	}
-	/////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////// 공지글 CRUD 끝 ///////////////////////////////////
-	/////////////////////////////////////////////////////////////////////////////////
 
+	///////////////////////////// 신고글 목록 CRUD 시작 /////////////////////////////
+	//신고글 목록 가져오기(select)   pageNo : 페이지 번호
+	public List<DeclBean> selectDeclList(int pageNo) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		pageNo = (pageNo-1)*5;
+		
+		try {
+			return sqlSession.selectList(namespace+"selectDeclList", pageNo); 
+		} finally {
+			sqlSession.close();
+		}
+	}
+	///////////////////////////// 신고글 목록 CRUD 끝 ///////////////////////////////
+
+	
+	///////////////////////////// 신고글 CRUD 시작//////////////////////////////////
+	// 신고 글 가져오기(select)
+	public DeclBean selectDecl(int bno) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+
+		try {
+			return sqlSession.selectOne(namespace+"selectDecl", bno);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	// 신고 글 삭제하기(delete)
+	public int deleteDecl(int bno) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+
+		try {
+			return sqlSession.delete(namespace+"deleteDecl", bno);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	///////////////////////////// 신고글 CRUD 끝 ///////////////////////////////////
+	
+	///////////////////////////// 책 소개페이지 CRUD 시작//////////////////////////////////
+	// 책 소개페이지 글 쓰기(insert)
+	public int insertBook(BookBean bean) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+
+		try {
+			return sqlSession.insert(namespace+"insertBook", bean);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	// 책 소개페이지 글 가져오기(select)
+	public BookBean selectBook(int bookNo) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+
+		try {
+			return sqlSession.selectOne(namespace+"selectBook", bookNo);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	// 책 소개페이지 수정하기(update)
+	public int updateBook(BookBean bean) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+
+		try {
+			return sqlSession.update(namespace+"updateBook", bean);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	// 책 소개페이지 삭제하기(delete)
+	public int deleteBook(int bookNo) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+
+		try {
+			return sqlSession.delete(namespace+"deleteBook", bookNo);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	///////////////////////////// 책 소개페이지 CRUD 끝 ///////////////////////////////////
 }
--- Proj_Quill_Pen/src/service/AdminService.java
+++ Proj_Quill_Pen/src/service/AdminService.java
@@ -1,32 +1,42 @@
 package service;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import repository.DTO.BoardBean;
 import repository.DTO.BookBean;
+import repository.DTO.DeclBean;
 import repository.DTO.NoticeBean;
 
 public interface AdminService {
 
 	// admin에서는 모든 메서드 시작 전 관리자가 맞는지 유효성 검사가 필요함!
 
+	// 공지글 목록
+	public List<NoticeBean> noticeList (HttpServletRequest req, HttpServletResponse resp) throws Exception;
+	//(int bno)
+	
+	// 신고글 목록
+	public List<DeclBean> reportList (HttpServletRequest req, HttpServletResponse resp) throws Exception;
+	// (int dno)
+	
 	// 공지 쓰기
-	public NoticeBean writeNotice(HttpServletRequest req, HttpServletResponse resp) throws Exception;
-	// public NoticeBean writeNotice (String mId, String title, String content)
-	// throws Exception;
-
+	public NoticeBean writeNotice (HttpServletRequest req, HttpServletResponse resp) throws Exception;
+	//public NoticeBean writeNotice (String mId, String title, String content) throws Exception;
+	
 	// 신고글 삭제
-	public BoardBean reportDel(HttpServletRequest req, HttpServletResponse resp) throws Exception;
-	// public BoardBean reportDel (int bno) throws Exception;
-
+	public BoardBean reportDel (HttpServletRequest req, HttpServletResponse resp) throws Exception;
+	//public BoardBean reportDel (int bno) throws Exception;
+	
 	// 책 소개 페이지 작성, 수정
-	public BookBean bookIntro(HttpServletRequest req, HttpServletResponse resp) throws Exception;
-	// public BookBean bookIntro (String fileName, String title, String subTitle,
-	// String imageName, String category, String link) throws Exception;
-
+	public BookBean bookIntro (HttpServletRequest req, HttpServletResponse resp) throws Exception;
+	//public BookBean bookIntro (String fileName, String title, String subTitle, String imageName, String category, String link) throws Exception;
+	
 	// 책 소개 페이지 삭제
-	public BookBean bookIntroDel(HttpServletRequest req, HttpServletResponse resp) throws Exception;
-	// public BookBean bookIntroDel (String title) throws Exception;
+	public BookBean bookIntroDel (HttpServletRequest req, HttpServletResponse resp) throws Exception;
+	//public BookBean bookIntroDel (String title) throws Exception;
+	
 
 }
