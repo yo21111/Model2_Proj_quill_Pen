da350b0758427bb574f4a1eeae1eba04e1bac06d
Lee H <test@naver.com>
2022-04-22 14:00:24 +0900

    test by H

--- Proj_Quill_Pen/src/controller/CommandHandler.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface CommandHandler {
-	public String process(HttpServletRequest req, HttpServletResponse resp) throws Exception;
-}
